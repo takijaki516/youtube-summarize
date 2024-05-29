cffda9fbff763737206b65f5fdeccc5e1ff29887
tk yang <takijaki516@gmail.com>
2024-05-29 12:18:25 +0900

    prisma & shadcn setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+const HomePage = async () => {
+  return (
+    <section>
+      <div>
+        <h1>SUMMARIZE YOUTUBE VIDEO</h1>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default HomePage;
